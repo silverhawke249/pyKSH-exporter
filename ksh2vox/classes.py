bf1602e9efbaa90e377a164c67f79f83e5088a0d
silverhawke249 <hi@silverhawke.xyz>
2023-04-07 21:22:33 +0700

    Implement BT and FX reading in a parser class

--- ksh2vox/classes.py
+++ ksh2vox/classes.py
@@ -1,8 +1,8 @@
 import dataclasses
-import decimal
-import enum
-import fractions
 
+from decimal import Decimal
+from enum import Enum
+from fractions import Fraction
 from typing import Any
 
 
@@ -10,16 +10,52 @@ class ParserWarning(UserWarning):
     pass
 
 
+# TODO: Figure this out later lol
+class GameBackground(Enum):
+    WHAT = 0
+
+
+class InfVer(Enum):
+    INFINITE = 2
+    GRAVITY  = 3
+    HEAVENLY = 4
+    VIVID    = 5
+    EXCEED   = 6
+
+
+@dataclasses.dataclass()
+class SongInfo:
+    title: str = ''
+    title_yomigana: str = ''
+    artist: str = ''
+    artist_yomigana: str = ''
+    ascii_label: str = ''
+    max_bpm: Decimal = Decimal(0)
+    min_bpm: Decimal = Decimal(0)
+    release_date: str = ''
+    music_volume: int = 100
+    background: GameBackground = GameBackground.WHAT
+    inf_ver: InfVer = InfVer.INFINITE
+
+
+class DifficultySlot(Enum):
+    NOVICE   = 1
+    ADVANCED = 2
+    EXHAUST  = 3
+    INFINITE = 4
+    MAXIMUM  = 5
+
+
 @dataclasses.dataclass(frozen=True, order=True)
 class TimePoint:
     measure: int
     _count: dataclasses.InitVar[int]
     _subdivision: dataclasses.InitVar[int]
-    position: fractions.Fraction = dataclasses.field(init=False)
+    position: Fraction = dataclasses.field(init=False)
 
     def __post_init__(self, _count, _subdivision):
         self.validate(_count, _subdivision)
-        object.__setattr__(self, 'position', fractions.Fraction(_count, _subdivision))
+        object.__setattr__(self, 'position', Fraction(_count, _subdivision))
 
     def validate(self, _count, _subdivision):
         if self.measure < 0:
@@ -32,15 +68,15 @@ class TimePoint:
 
 @dataclasses.dataclass
 class BTInfo:
-    _duration: dataclasses.InitVar[fractions.Fraction | int]
-    duration: fractions.Fraction = dataclasses.field(init=False)
+    _duration: dataclasses.InitVar[Fraction | int]
+    duration: Fraction = dataclasses.field(init=False)
 
     def _setattrhook(self, __name: str, __value: Any):
         super().__setattr__(__name, __value)
         self.validate()
 
     def __post_init__(self, _duration):
-        self.duration = fractions.Fraction(_duration)
+        self.duration = Fraction(_duration)
         self.validate()
         self.__setattr__ = self._setattrhook
 
@@ -51,8 +87,8 @@ class BTInfo:
 
 @dataclasses.dataclass
 class FXInfo:
-    _duration: dataclasses.InitVar[fractions.Fraction | int]
-    duration: fractions.Fraction = dataclasses.field(init=False)
+    _duration: dataclasses.InitVar[Fraction | int]
+    duration: Fraction = dataclasses.field(init=False)
     special: int
 
     def _setattrhook(self, __name: str, __value: Any):
@@ -60,7 +96,7 @@ class FXInfo:
         self.validate()
 
     def __post_init__(self, _duration):
-        self.duration = fractions.Fraction(_duration)
+        self.duration = Fraction(_duration)
         self.validate()
         self.__setattr__ = self._setattrhook
 
@@ -71,21 +107,22 @@ class FXInfo:
             raise ValueError(f'special must be positive (got {self.special})')
 
 
-class FilterType(enum.Enum):
+# TODO: redo this with the proper index (1 = lpf, 2 = hpf, 3 = bitcrush)
+class FilterType(Enum):
     PEAK = 0
     LPF = 2
     HPF = 4
     BITCRUSH = 5
 
 
-class SpinType(enum.Enum):
+class SpinType(Enum):
     NO_SPIN = 0
     SINGLE_SPIN = 1
     TRIPLE_SPIN = 4
     HALF_SPIN = 5
 
 
-class EasingType(enum.Enum):
+class EasingType(Enum):
     NO_EASING = 0
     LINEAR = 2
     EASE_IN_SINE = 4
@@ -94,8 +131,8 @@ class EasingType(enum.Enum):
 
 @dataclasses.dataclass
 class VolInfo:
-    start: fractions.Fraction
-    end: fractions.Fraction
+    start: Fraction
+    end: Fraction
     spin_type: SpinType = SpinType.NO_SPIN
     spin_duration: int = 0
     ease_type: EasingType = EasingType.NO_EASING
@@ -140,7 +177,7 @@ class NoteData:
     vol_r: dict[TimePoint, VolInfo] = dataclasses.field(default_factory=dict)
 
 
-class FXType(enum.Enum):
+class FXType(Enum):
     NO_EFFECT   = 0
     RETRIGGER_1 = 1
     GATE        = 2
@@ -152,7 +189,7 @@ class FXType(enum.Enum):
     RETRIGGER_2 = 8
     PITCH_SHIFT = 9
     TAPESCRATCH = 10
-    # UNKNOWN     = 11 (we'll figure this out some day)
+    LPF         = 11 # fuck around with this later (it's like 11, 75.00, 400.00, 900.00, 2.00)
     HPF         = 12
 
 
@@ -164,19 +201,19 @@ class FXParameters:
 @dataclasses.dataclass
 class FilterFXInfo:
     which: int
-    duration: fractions.Fraction
+    duration: Fraction
 
 
 @dataclasses.dataclass
 class SPControllerInfo:
-    value: decimal.Decimal
+    value: Decimal
     is_new_segment: bool = True
 
 
 @dataclasses.dataclass
 class SPControllerData:
-    zoom_bottom: dict[TimePoint, SPControllerInfo] = dataclasses.field(default_factory=dict)
     zoom_top   : dict[TimePoint, SPControllerInfo] = dataclasses.field(default_factory=dict)
+    zoom_bottom: dict[TimePoint, SPControllerInfo] = dataclasses.field(default_factory=dict)
 
     tilt: dict[TimePoint, SPControllerInfo] = dataclasses.field(default_factory=dict)
 
@@ -200,8 +237,21 @@ class TimeSignature:
 
 @dataclasses.dataclass()
 class ChartInfo:
-    bpms     : dict[TimePoint, decimal.Decimal] = dataclasses.field(default_factory=dict)
-    time_sigs: dict[TimePoint, TimeSignature]   = dataclasses.field(default_factory=dict)
+    # Metadata stuff
+    level      : int = 1
+    difficulty : DifficultySlot = DifficultySlot.MAXIMUM
+    effector   : str = 'dummy'
+    illustrator: str = 'dummy'
+
+    # To be used by converters
+    music_path   : str = ''
+    music_offset : int = 0
+    preview_start: int = 0
+    jacket_path  : str = ''
+
+    # Song data that may change mid-song
+    bpms     : dict[TimePoint, Decimal] = dataclasses.field(default_factory=dict)
+    time_sigs: dict[TimePoint, TimeSignature] = dataclasses.field(default_factory=dict)
 
     # Effect into
     fx_list     : list[FXParameters]            = dataclasses.field(default_factory=list)
@@ -213,3 +263,9 @@ class ChartInfo:
 
     # SPController data
     spcontroller_data: SPControllerData = dataclasses.field(default_factory=SPControllerData)
+
+    def __post_init__(self):
+        # Default values
+        self.bpms[TimePoint(1, 0, 1)]         = Decimal(120)
+        self.time_sigs[TimePoint(1, 0, 1)]    = TimeSignature()
+        self.filter_types[TimePoint(1, 0, 1)] = FilterType.PEAK
--- ksh2vox/reader/ksh.py
+++ ksh2vox/reader/ksh.py
@@ -1,16 +1,20 @@
 import dataclasses
-import io
 import re
 import warnings
 
+from decimal import Decimal
 from fractions import Fraction
+from pathlib import Path
 from typing import TextIO
 
 from ..classes import (
     BTInfo,
     ChartInfo,
+    DifficultySlot,
+    FilterType,
     FXInfo,
     ParserWarning,
+    SongInfo,
     SpinType,
     TimePoint,
     TimeSignature,
@@ -21,6 +25,15 @@ BAR_LINE = '--'
 CHART_REGEX = re.compile(r'^[012]{4}\|[012]{2}\|[0-9A-Za-o-:]{2}(?:(@(\(|\)|<|>)|S>|S<)\d+)?')
 LASER_POSITION = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmno'
 FX_STATE_MAP = {'0': '0', '1': '2', '2': '1'}
+INPUT_BT  = ['bt_a', 'bt_b', 'bt_c', 'bt_d']
+INPUT_FX  = ['fx_l', 'fx_r']
+INPUT_VOL = ['vol_l', 'vol_r']
+FILTER_TYPE_MAP = {
+    'peak': FilterType.PEAK,
+    'lpf1': FilterType.LPF,
+    'hpf1': FilterType.HPF,
+    'bitc': FilterType.BITCRUSH,
+}
 
 
 @dataclasses.dataclass
@@ -36,15 +49,67 @@ class _LastVolInfo:
     prev_vol: VolInfo
 
 
-@dataclasses.dataclass(eq=False, )
+# Song metadata in XML:
+# - ID
+# - Title
+# - Title (yomigana)
+# - Artist
+# - Artist (yomigana)
+# - ASCII label
+# - Max BPM
+# - Min BPM
+# - Release date
+# - Volume
+# - In-game background
+# - Genre (FLOOR/TOUHOU/etc)
+# - is_fixed (?)
+# - Version (1-6)
+# - demo_pri (show this in attract screen?)
+# - inf_ver (INF/GRV/HVN/VVD/XCD)
+# We can only derive title/artist/BPM info from KSH, the rest must be user input
+
+# Chart metadata in XML:
+# - Level
+# - Illustrator
+# - Effector
+# - Price (-1)
+# - Limited (3)
+
+
+@dataclasses.dataclass(eq=False)
 class KSHParser:
+    # Init variables
     file: dataclasses.InitVar[TextIO]
 
-    _raw_metadata: list[str] = dataclasses.field(init=False)
-    _raw_notedata: list[str] = dataclasses.field(init=False)
-    _raw_definitions: list[str] = dataclasses.field(init=False)
+    # Private variables
+    _ksh_path: Path = dataclasses.field(init=False)
+
+    _raw_metadata: list[str] = dataclasses.field(init=False, repr=False)
+    _raw_notedata: list[str] = dataclasses.field(init=False, repr=False)
+    _raw_definitions: list[str] = dataclasses.field(init=False, repr=False)
+
+    _song_info : SongInfo  = dataclasses.field(init=False)
+    _chart_info: ChartInfo = dataclasses.field(init=False)
+
+    _fx_list: list[str]            = dataclasses.field(default_factory=list, init=False, repr=False)
+
+    # Stateful data for notechart parsing
+    _cur_timesig: TimeSignature = dataclasses.field(default=TimeSignature(), init=False, repr=False)
+    _cur_filter : FilterType    = dataclasses.field(default=FilterType.PEAK, init=False, repr=False)
+
+    _filter_changed: bool = dataclasses.field(default=False, init=False, repr=False)
+
+    _holds : dict[str, _HoldInfo] = dataclasses.field(default_factory=dict, init=False, repr=False)
+    _set_fx: dict[str, str]       = dataclasses.field(default_factory=dict, init=False, repr=False)
+    _set_se: dict[str, int]       = dataclasses.field(default_factory=dict, init=False, repr=False)
+
+    _bts : dict[str, dict[TimePoint, BTInfo]]  = dataclasses.field(init=False, repr=False)
+    _fxs : dict[str, dict[TimePoint, FXInfo]]  = dataclasses.field(init=False, repr=False)
+    _vols: dict[str, dict[TimePoint, VolInfo]] = dataclasses.field(init=False, repr=False)
 
     def __post_init__(self, file: TextIO):
+        self._ksh_path = Path(file.name).resolve()
+
         self._raw_metadata = []
         for line in file:
             line = line.strip()
@@ -55,13 +120,272 @@ class KSHParser:
         self._raw_notedata = [line.strip() for line in file]
 
         self._raw_definitions = []
-        for line in reversed(self._raw_notedata):
-            self._raw_notedata.pop()
+        while True:
+            line = self._raw_notedata[-1]
             if line == BAR_LINE:
                 break
             self._raw_definitions.append(line)
+            self._raw_notedata.pop()
 
         self._raw_definitions.reverse()
+
+        self._parse_metadata()
+        self._parse_notedata()
+        self._parse_definitions()
+
+    def _parse_metadata(self) -> None:
+        self._song_info = SongInfo()
+        self._chart_info = ChartInfo()
+
+        for line_no, line in enumerate(self._raw_metadata):
+            if '=' not in line:
+                warnings.warn(f'unrecognized line at line {line_no + 1}: {line}')
+            key, value = line.split('=', 1)
+            if key == 'title':
+                self._song_info.title = value
+            elif key == 'artist':
+                self._song_info.artist = value
+            elif key == 'effect':
+                self._chart_info.effector = value
+            elif key == 'jacket':
+                self._chart_info.jacket_path = value
+            elif key == 'illustrator':
+                self._chart_info.illustrator = value
+            elif key == 'difficulty':
+                if value == 'light':
+                    self._chart_info.difficulty = DifficultySlot.NOVICE
+                elif value == 'challenge':
+                    self._chart_info.difficulty = DifficultySlot.ADVANCED
+                elif value == 'extended':
+                    self._chart_info.difficulty = DifficultySlot.EXHAUST
+                else:
+                    self._chart_info.difficulty = DifficultySlot.MAXIMUM
+            elif key == 'level':
+                self._chart_info.level = int(value)
+            elif key == 't':
+                if '-' in value:
+                    min_bpm_str, max_bpm_str = value.split('-')
+                    self._song_info.min_bpm = Decimal(min_bpm_str)
+                    self._song_info.max_bpm = Decimal(max_bpm_str)
+                else:
+                    bpm = Decimal(value)
+                    self._song_info.min_bpm = bpm
+                    self._song_info.max_bpm = bpm
+                    self._chart_info.bpms[TimePoint(1, 0, 1)] = bpm
+            elif key == 'beat':
+                upper_str, lower_str = value.split('/')
+                upper, lower = int(upper_str), int(lower_str)
+                self._chart_info.time_sigs[TimePoint(1, 0, 1)] = TimeSignature(upper, lower)
+                self._cur_timesig = TimeSignature(upper, lower)
+            elif key == 'm':
+                self._chart_info.music_path = value
+            elif key == 'mvol':
+                self._song_info.music_volume = int(value)
+            elif key == 'o':
+                self._chart_info.music_offset = int(value)
+            elif key == 'po':
+                self._chart_info.preview_start = int(value)
+            elif key == 'filtertype':
+                if value == 'peak':
+                    self._chart_info.filter_types[TimePoint(1, 0, 1)] = FilterType.PEAK
+                elif value == 'hpf1':
+                    self._chart_info.filter_types[TimePoint(1, 0, 1)] = FilterType.HPF
+                elif value == 'lpf1':
+                    self._chart_info.filter_types[TimePoint(1, 0, 1)] = FilterType.LPF
+                elif value == 'bitc':
+                    self._chart_info.filter_types[TimePoint(1, 0, 1)] = FilterType.BITCRUSH
+            elif key == 'ver':
+                # You know, I should probably differentiate handling top/bottom zooms depending if
+                # the version is >= 167 or not, but I'm most likely not going to fucking bother
+                try:
+                    version = int(value)
+                    if version < 160:
+                        raise ValueError(f'ksh file version too old (got {value})')
+                except ValueError as e:
+                    raise ValueError(f'ksh file version too old (got {value})') from e
+            else:
+                # Ignoring all other metadata
+                pass
+
+    def _parse_notedata(self) -> None:
+        # Initialize these for chart parsing
+        self._bts = {
+            'bt_a': {},
+            'bt_b': {},
+            'bt_c': {},
+            'bt_d': {},
+        }
+        self._fxs = {
+            'fx_l': {},
+            'fx_r': {},
+        }
+        self._vols = {
+            'vol_l': {},
+            'vol_r': {},
+        }
+
+        # Measure data
+        line_no_offset   : int       = len(self._raw_metadata) + 1
+        measure_data     : list[str] = []
+        measure_number   : int       = 1
+        subdivision_count: int       = 0
+        for line_no, line in enumerate(self._raw_notedata):
+            # Four types of lines in here:
+            # 1. Note data
+            match = CHART_REGEX.search(line)
+            if match is not None:
+                measure_data.append(match.group(0))
+                subdivision_count += 1
+            # 2. Metadata (BPM change, time signature change, FX specifier, etc)
+            # Metadata is processed together with note data
+            elif '=' in line:
+                measure_data.append(line)
+            # 3. Comments
+            # Comments will be used to give extra data that's not supported by KSM
+            elif line.startswith('//'):
+                measure_data.append(line)
+            # 4. Measure divider
+            elif line == BAR_LINE:
+                self._parse_measure(measure_data, measure_number, subdivision_count)
+                measure_number   += 1
+                measure_data      = []
+                subdivision_count = 0
+            else:
+                warnings.warn(f'unrecognized line at line {line_no_offset + line_no + 1}: {line}')
+
+    def _parse_measure(self, measure: list[str], m_no: int, m_linecount: int) -> None:
+        # Check time signatures that get pushed to the next measure
+        if TimePoint(m_no, 0, 1) in self._chart_info.time_sigs:
+            self._cur_timesig = self._chart_info.time_sigs[TimePoint(m_no, 0, 1)]
+
+        noteline_count = 0
+        for line in measure:
+            subdivision = Fraction(1 * self._cur_timesig.upper, m_linecount * self._cur_timesig.lower)
+            cur_subdiv  = noteline_count * subdivision
+            cur_time    = TimePoint(m_no, cur_subdiv.numerator, cur_subdiv.denominator)
+
+            # 1. Metadata
+            if '=' in line:
+                key, value = line.split('=', 1)
+                if key == 't':
+                    pass
+                elif key == 'beat':
+                    pass
+                elif key == 'stop':
+                    pass
+                elif key == 'tilt':
+                    pass
+                elif key == 'zoom_top':
+                    pass
+                elif key == 'zoom_bottom':
+                    pass
+                elif key == 'center_split':
+                    pass
+                elif key in ['laserrange_l', 'laserrange_r']:
+                    pass
+                elif key in ['fx-l', 'fx-r']:
+                    key = key.replace('-', '_')
+                    if value and value not in self._fx_list:
+                        self._fx_list.append(value)
+                    if key in self._set_fx:
+                        # Send warning only if:
+                        # - effect is not null
+                        # - currently stored effect is not the null effect
+                        # - currently stored effect is different from incoming effect
+                        if value and self._set_fx[key] and self._set_fx[key] != value:
+                            warnings.warn(f'ignoring effect {value} assigned to {key} that already has an assigned '
+                                          f'effect {self._set_fx[key]} at m{m_no}', ParserWarning)
+                        continue
+                    self._set_fx[key] = value
+                elif key in ['fx-l_se', 'fx-r_se']:
+                    key = key[:4]
+                    key = key.replace('-', '_')
+                    value = value.split(';')[0]
+                    if value.endswith('.wav'):
+                        value = value[:-4]
+                    try:
+                        self._set_se[key] = int(value)
+                    except ValueError:
+                        pass
+                elif key == 'filtertype':
+                    if value in FILTER_TYPE_MAP:
+                        self._cur_filter = FILTER_TYPE_MAP[value]
+                        self._filter_changed = True
+                    else:
+                        # Handle track auto tab here
+                        pass
+                elif ':' in key:
+                    # This is for filter settings
+                    # > filter:[filter_name]:[parameter]=[value]
+                    # Technically FX parameters can also be changed here but like no one uses that
+                    pass
+                else:
+                    # Ignoring all other metadata
+                    pass
+            # 2. Comment
+            elif line.startswith('//'):
+                # Remove initial `//`
+                line = line[2:]
+                if '=' in line:
+                    # Define custom commands here
+                    pass
+            # 3. Notedata
+            else:
+                bts, fxs, vols_and_spin = line.split('|')
+                vols = vols_and_spin[:2]
+                spin = vols_and_spin[2:]
+                for bt, state in zip(INPUT_BT, bts):
+                    if state == '0' and bt in self._holds:
+                        self._bts[bt][self._holds[bt].start] = BTInfo(self._holds[bt].duration)
+                        del self._holds[bt]
+                    if state == '1':
+                        if bt in self._holds:
+                            warnings.warn(f'improperly terminated hold at {cur_time}', ParserWarning)
+                            del self._holds[bt]
+                        self._bts[bt][cur_time] = BTInfo(0)
+                    if state == '2':
+                        if bt not in self._holds:
+                            self._holds[bt] = _HoldInfo(cur_time)
+                        self._holds[bt].duration += subdivision
+                for fx, state in zip(INPUT_FX, fxs):
+                    if state == '0' and fx in self._holds:
+                        fx_effect = self._set_fx.get(fx, None)
+                        if fx in self._set_fx:
+                            del self._set_fx[fx]
+                        if not fx_effect:
+                            fx_index = 0
+                        else:
+                            fx_index = self._fx_list.index(fx_effect) + 2
+                        self._fxs[fx][self._holds[fx].start] = FXInfo(self._holds[fx].duration, fx_index)
+                        del self._holds[fx]
+                    if state == '1':
+                        if fx not in self._holds:
+                            self._holds[fx] = _HoldInfo(cur_time)
+                        self._holds[fx].duration += subdivision
+                    if state == '2':
+                        if fx in self._holds:
+                            warnings.warn(f'improperly terminated hold at {cur_time}', ParserWarning)
+                            del self._holds[fx]
+                        se_index = self._set_se.get(fx, 0)
+                        self._fxs[fx][cur_time] = FXInfo(0, se_index)
+                    # Clean up -- FX SE only affects the FX chip immediately after
+                    if fx in self._set_se:
+                        del self._set_se[fx]
+                for vol, state in zip(INPUT_VOL, vols):
+                    pass
+
+                noteline_count += 1
+
+        # Store note data in chart
+        for k, v1 in self._bts.items():
+            setattr(self._chart_info.note_data, k, v1)
+        for k, v2 in self._fxs.items():
+            setattr(self._chart_info.note_data, k, v2)
+        for k, v3 in self._vols.items():
+            setattr(self._chart_info.note_data, k, v3)
+
+    def _parse_definitions(self):
+        pass
 
 
 def process_ksh_line(s: str) -> dict[str, str]:
@@ -79,7 +403,7 @@ def process_ksh_line(s: str) -> dict[str, str]:
     }
 
 
-def read_ksh(f: io.TextIOBase) -> ChartInfo:
+def read_ksh(f: TextIO) -> ChartInfo:
     chart = ChartInfo()
     chart.time_sigs[TimePoint(0, 0, 1)] = TimeSignature()
 
