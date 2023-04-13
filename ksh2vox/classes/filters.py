4b6b15032b5d9a998ef86b7334e90bb5ab24b6fe
silverhawke249 <hi@silverhawke.xyz>
2023-04-13 16:07:49 +0700

    Implement filter classes

---
+++ ksh2vox/classes/filters.py
@@ -0,0 +1,69 @@
+from abc import ABC, abstractmethod, abstractproperty
+from dataclasses import dataclass
+from enum import Enum
+
+class FilterType(Enum):
+    PEAK     = 0
+    LPF      = 1
+    HPF      = 2
+    BITCRUSH = 3
+
+@dataclass
+class Filter(ABC):
+    @abstractproperty
+    def filter_index(self) -> FilterType:
+        pass
+
+    @abstractmethod
+    def to_vox_string(self) -> str:
+        pass
+
+@dataclass
+class LowpassFilter(Filter):
+    mix: float = 90.00
+    min_cutoff: float = 400.00
+    max_cutoff: float = 18000.00
+    bandwidth: float = 0.70
+
+    @property
+    def filter_index(self) -> FilterType:
+        return FilterType.LPF
+
+    def to_vox_string(self) -> str:
+        return ',\t'.join([f'{self.filter_index.value}',
+                           f'{self.mix:.2}',
+                           f'{self.min_cutoff:.2}',
+                           f'{self.max_cutoff:.2}',
+                           f'{self.bandwidth:.2}'])
+
+@dataclass
+class HighpassFilter(Filter):
+    mix: float = 90.00
+    min_cutoff: float = 40.00
+    max_cutoff: float = 5000.00
+    bandwidth: float = 0.70
+
+    @property
+    def filter_index(self) -> FilterType:
+        return FilterType.HPF
+
+    def to_vox_string(self) -> str:
+        return ',\t'.join([f'{self.filter_index.value}',
+                           f'{self.mix:.2}',
+                           f'{self.min_cutoff:.2}',
+                           f'{self.max_cutoff:.2}',
+                           f'{self.bandwidth:.2}'])
+
+@dataclass
+class BitcrushFilter(Filter):
+    mix: float = 100.00
+    max_amount: int = 30
+
+    @property
+    def filter_index(self) -> FilterType:
+        return FilterType.BITCRUSH
+
+    def to_vox_string(self) -> str:
+        return ',\t'.join([f'{self.filter_index.value}',
+                           f'{self.mix:.2}',
+                           f'{self.max_amount}'])
