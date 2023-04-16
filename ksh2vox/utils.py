2c4f1ee3c90da6788aee694c248aa7b58e24cec8
silverhawke249 <hi@silverhawke.xyz>
2023-04-16 21:18:18 +0700

    Add parsing functions

--- ksh2vox/utils.py
+++ ksh2vox/utils.py
@@ -29,3 +29,52 @@ def interpolate(
         return initial_value + (sin((count / total - 1) * pi / 2) + 1) * difference
     else:
         raise ValueError(f'invalid ease type (got {ease_type})')
+
+
+def parse_length(
+    s: str
+) -> float:
+    try:
+        return float(s)
+    except ValueError as e:
+        if '/' in s:
+            num, denom = s.split('/')
+            return float(num) / float(denom)
+        elif s.endswith('%'):
+            return float(s[:-1]) / 100
+        elif s.endswith('s'):
+            raise ValueError(f's/ms units are not supported (got {s})') from e
+        raise ValueError(f'invalid format (got {s})') from e
+
+
+def parse_decibel(
+    s: str
+) -> float:
+    if not s.endswith('dB'):
+        raise ValueError(f'{s} is not a valid decibel value')
+    else:
+        return float(s[:-2])
+
+
+def parse_frequency(
+    s: str
+) -> float:
+    if not s.endswith('Hz'):
+        raise ValueError(f'{s} is not a valid frequency value')
+    else:
+        s = s[:-2]
+        if s.endswith('k'):
+            return float(s[:-1]) * 1000
+        return float(s)
+
+
+def parse_time(
+    s: str
+) -> float:
+    if not s.endswith('s'):
+        raise ValueError(f'{s} is not a valid time value')
+    else:
+        s = s[:-1]
+        if s.endswith('m'):
+            return float(s[:-1])
+        return float(s) * 1000
