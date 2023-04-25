d89c2f0eae4f7d3b28b12920fc50f966097f755a
silverhawke249 <hi@silverhawke.xyz>
2023-04-25 11:46:54 +0700

    Complete image resize routine

--- ksh2vox/media/images.py
+++ ksh2vox/media/images.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 from pathlib import Path
 
 from PIL import Image
@@ -7,4 +8,16 @@ def get_jacket_images(file_path: Path) -> tuple[bytes, bytes, bytes]:
     """ Return jacket images as PNG blobs at regular, big, small sizes in order. """
     image = Image.open(str(file_path))
 
-    return b'', b'', b''
+    image_r = image.resize((300, 300), Image.Resampling.BICUBIC)
+    image_b = image.resize((676, 676), Image.Resampling.BICUBIC)
+    image_s = image.resize((108, 108), Image.Resampling.BICUBIC)
+
+    fobj_r = BytesIO()
+    fobj_b = BytesIO()
+    fobj_s = BytesIO()
+
+    image_r.save(fobj_r, format='png')
+    image_b.save(fobj_b, format='png')
+    image_s.save(fobj_s, format='png')
+
+    return fobj_r.getvalue(), fobj_b.getvalue(), fobj_s.getvalue()
