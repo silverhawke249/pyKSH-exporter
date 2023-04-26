5e3c286d49321851f950a5b5359d0adf9136cdd3
silverhawke249 <hi@silverhawke.xyz>
2023-04-26 23:49:48 +0700

    Refer to the resampling constants correctly

--- ksh2vox/media/images.py
+++ ksh2vox/media/images.py
@@ -8,9 +8,9 @@ def get_jacket_images(file_path: Path) -> tuple[bytes, bytes, bytes]:
     """ Return jacket images as PNG blobs at regular, big, small sizes, in that order. """
     image = Image.open(str(file_path))
 
-    image_r = image.resize((300, 300), Image.Resampling.BICUBIC)
-    image_b = image.resize((676, 676), Image.Resampling.BICUBIC)
-    image_s = image.resize((108, 108), Image.Resampling.BICUBIC)
+    image_r = image.resize((300, 300), Image.BICUBIC)
+    image_b = image.resize((676, 676), Image.BICUBIC)
+    image_s = image.resize((108, 108), Image.BICUBIC)
 
     fobj_r = BytesIO()
     fobj_b = BytesIO()
