197ccfc371091b4717a79377c180a1d9318c4789
silverhawke249 <hi@silverhawke.xyz>
2023-06-13 17:34:46 +0700

    More baseline work done

--- get_notecount.py
+++ get_notecount.py
@@ -1,9 +1,12 @@
+import warnings
+import re
+
 from argparse import ArgumentParser
 from dataclasses import dataclass, field
 from decimal import Decimal
 from enum import Enum, IntEnum, auto
 
-from ksh2vox.classes.base import TimePoint, TimeSignature
+from ksh2vox.classes.base import TimePoint, TimeSignature, ParserWarning
 
 
 class ParserState(IntEnum):
@@ -22,34 +25,83 @@ class ButtonType(IntEnum):
     D = 6
     R = 7
 
+BUTTON_MAPPER = {
+    '2': ButtonType.L,
+    '3': ButtonType.A,
+    '4': ButtonType.B,
+    '5': ButtonType.C,
+    '6': ButtonType.D,
+    '7': ButtonType.R,
+}
+
 
 @dataclass
 class VOXInfo():
-    time_sigs: dict[TimePoint, TimeSignature]
-    bpms     : dict[TimePoint, Decimal]
-    chips    : dict[tuple[TimePoint, ButtonType], int]
-    longs    : dict[tuple[TimePoint, ButtonType], int]
-    # lasers   : dict[TimePoint, int]
+    timesigs: dict[TimePoint, TimeSignature]          = field(default_factory=dict)
+    bpms    : dict[TimePoint, Decimal]                = field(default_factory=dict)
+    buttons : dict[tuple[TimePoint, ButtonType], int] = field(default_factory=dict)
+    # lasers  : dict[TimePoint, int]
 
 
-def main(infile: str):
-    with open(infile, 'r') as f:
-        parser_state = ParserState.Nothing
-        for line in f:
-            line = line.strip()
-            if line == '#BEAT INFO':
-                parser_state = ParserState.BeatInfo
-            elif line == '#BPM INFO':
-                parser_state = ParserState.BPMInfo
-            elif line.startswith('#TRACK') and len(line) == 7:
-                parser_state = ParserState.TrackInfo
-            elif line == '#END':
-                parser_state = ParserState.Nothing
-            elif line.startswith('#'):
-                parser_state = ParserState.OtherInfo
-            else:
+class Main():
+    vox_data: VOXInfo = VOXInfo()
+
+    parser_state: ParserState
+
+    cur_bpm    : Decimal
+    cur_timesig: TimeSignature
+
+    def __init__(self, infile: str):
+        self.vox_data     = VOXInfo()
+        self.parser_state = ParserState.Nothing
+        self.cur_bpm      = Decimal(120)
+        self.cur_timesig  = TimeSignature()
+
+        with open(infile, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line == '#BEAT INFO':
+                    self.parser_state = ParserState.BeatInfo
+                elif line == '#BPM INFO':
+                    self.parser_state = ParserState.BPMInfo
+                elif line.startswith('#TRACK') and len(line) == 7:
+                    self.parser_state = ParserState.TrackInfo
+                elif line == '#END':
+                    self.parser_state = ParserState.Nothing
+                elif line.startswith('#'):
+                    self.parser_state = ParserState.OtherInfo
+                else:
+                    try:
+                        self.handle_line(line)
+                    except ValueError:
+                        warnings.warn(f'invalid line (got {line})', ParserWarning)
+
+    def str_to_timepoint(self, time_str: str) -> TimePoint:
+        return TimePoint(0, 0, 1)
+
+    def handle_line(self, line):
+        line_parts = re.split(r'\s+', line)
+        if len(line_parts) < 2:
+            warnings.warn(f'invalid line (got {line})', ParserWarning)
+            return
+
+        time_str, *data = line_parts
+        timepoint = self.str_to_timepoint(time_str)
+        if self.parser_state == ParserState.BeatInfo:
+            self.cur_timesig = TimeSignature(int(data[0]), int(data[1]))
+            self.vox_data.timesigs[timepoint] = self.cur_timesig
+        elif self.parser_state == ParserState.BPMInfo:
+            self.cur_bpm = Decimal(data[0])
+            self.vox_data.bpms[timepoint] = self.cur_bpm
+        elif self.parser_state == ParserState.TrackInfo:
+            if line[-1] in ['1', '8']:
                 pass
-
+            elif '1' < line[-1] < '8':
+                self.vox_data.buttons[timepoint, BUTTON_MAPPER[line[-1]]] = int(data[0])
+            else:
+                warnings.warn(f'invalid track section (got {line})', ParserWarning)
+        else:
+            pass
 
 
 if __name__ == '__main__':
