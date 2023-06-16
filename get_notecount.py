3e8b0e61c7ecb1cb8b47a9a3bcd6d6717bb84740
silverhawke249 <hi@silverhawke.xyz>
2023-06-16 16:26:00 +0700

    Clean up, progress on figuring out hold ticks

--- get_notecount.py
+++ get_notecount.py
@@ -139,23 +139,26 @@ class Parser():
 
         return duration
 
-    def get_active_bpm(self, tick: Tickcount) -> Decimal:
+    def get_active_bpm(self, timepoint: Tickcount) -> Decimal:
         prev_bpm = Decimal(0)
         for time, bpm in self.vox_data.bpms.items():
-            if time > tick:
+            if time > timepoint:
                 break
             prev_bpm = bpm
 
         return prev_bpm
 
-    def get_active_timesig(self, tick: Tickcount) -> TimeSignature:
+    def get_active_timesig(self, timepoint: Tickcount) -> TimeSignature:
         prev_timesig = TimeSignature()
         for time, timesig in self.vox_data.timesigs.items():
-            if time > tick:
+            if time > timepoint:
                 break
             prev_timesig = timesig
 
         return prev_timesig
+
+    def get_active_tickrate(self, timepoint: Tickcount) -> int:
+        return 24 if self.get_active_bpm(timepoint) >= 256 else 12
 
     def handle_line(self, line) -> None:
         if self.parser_state in [ParserState.Nothing, ParserState.OtherInfo]:
@@ -167,13 +170,13 @@ class Parser():
             return
 
         time_str, *data = line_parts
-        tick = self.str_to_tick(time_str)
+        timepoint = self.str_to_tick(time_str)
         if self.parser_state == ParserState.BeatInfo:
             self.cur_timesig = TimeSignature(int(data[0]), int(data[1]))
-            self.vox_data.timesigs[tick] = self.cur_timesig
+            self.vox_data.timesigs[timepoint] = self.cur_timesig
         elif self.parser_state == ParserState.BPMInfo:
             self.cur_bpm = Decimal(data[0])
-            self.vox_data.bpms[tick] = self.cur_bpm
+            self.vox_data.bpms[timepoint] = self.cur_bpm
         elif self.parser_state == ParserState.TrackInfo:
             if self.cur_track not in TRACK_MAP:
                 warnings.warn(f'invalid track section (got {self.cur_track})', ParserWarning)
@@ -183,36 +186,38 @@ class Parser():
             if isinstance(track_type, LaserType):
                 # Handle start/endpoints of lasers
                 if data[1] != '0':
-                    self.vox_data.lasers[track_type].append(tick)
+                    self.vox_data.lasers[track_type].append(timepoint)
                 # Handle slams
-                if self.prev_laser[track_type] == tick:
-                    self.vox_data.slams[track_type].add(tick)
-                self.prev_laser[track_type] = tick
+                if self.prev_laser[track_type] == timepoint:
+                    self.vox_data.slams[track_type].add(timepoint)
+                self.prev_laser[track_type] = timepoint
             elif isinstance(track_type, ButtonType):
-                self.vox_data.buttons[tick, track_type] = int(data[0])
+                self.vox_data.buttons[timepoint, track_type] = int(data[0])
             else:
                 pass
         else:
             pass
 
     def calculate_notes(self) -> None:
-        for key, duration in self.vox_data.buttons.items():
+        for key, value in self.vox_data.buttons.items():
             # Chip notes
-            if duration == 0:
+            if value == 0:
                 self.chip_count += 1
                 continue
             # Long notes
-            # TODO: Figure out what's wrong with this
-            tick, _ = key
-            cur_bpm = self.get_active_bpm(tick)
-            tick_rate = 24 if cur_bpm >= 256 else 12
-            remainder = 0
-            if tick % tick_rate != 0:
-                print(key, duration)
-                remainder = tick_rate - (tick % tick_rate)
-                self.long_count += 1
+            timepoint, _ = key
+            tick_rate = self.get_active_tickrate(timepoint)
+            cur_timept = timepoint
+            # Add ticks until it exceeds endpoint
             # TODO: Factor in BPM changes
-            self.long_count += (duration - remainder) // tick_rate
+            while cur_timept < timepoint + value:
+                if cur_timept % tick_rate != 0:
+                    cur_timept = (cur_timept // tick_rate) * tick_rate
+                cur_timept += tick_rate
+                self.long_count += 1
+            # TODO: Figure out why some holds lose ticks
+            if value >= tick_rate * 8:
+                self.long_count -= 2
         # Lasers
         laser_ticks: dict[LaserType, list[Tickcount]] = {
             LaserType.L: [],
@@ -220,14 +225,11 @@ class Parser():
         }
         for laser_type, laser_timepts in self.vox_data.lasers.items():
             for laser_s, laser_e in zip(*[iter(laser_timepts)] * 2):
+                tick_rate = self.get_active_tickrate(timepoint)
+                cur_timept = laser_s
                 # Round to next tick
-                cur_bpm = self.get_active_bpm(laser_s)
-                tick_rate = 24 if cur_bpm >= 256 else 12
-                if laser_s % tick_rate != 0:
-                    remainder = tick_rate - (laser_s % tick_rate)
-                    cur_timept = laser_s + remainder
-                else:
-                    cur_timept = laser_s
+                if cur_timept % tick_rate != 0:
+                    cur_timept += tick_rate - (laser_s % tick_rate)
                 # Add ticks until it exceeds endpoint
                 # TODO: Factor in BPM changes
                 while cur_timept < laser_e:
@@ -240,17 +242,16 @@ class Parser():
             LaserType.R: [],
         }
         for laser_type, slam_timepts in self.vox_data.slams.items():
-            for tick in slam_timepts:
+            for timepoint in slam_timepts:
                 # Round to previous tick
-                cur_bpm = self.get_active_bpm(tick)
-                tick_rate = 24 if cur_bpm >= 256 else 12
-                if tick % tick_rate != 0:
-                    tick -= tick % tick_rate
-                if tick in laser_ticks[laser_type]:
-                    if tick in slam_overlapping_ticks[laser_type]:
+                tick_rate = self.get_active_tickrate(timepoint)
+                if timepoint % tick_rate != 0:
+                    timepoint -= timepoint % tick_rate
+                if timepoint in laser_ticks[laser_type]:
+                    if timepoint in slam_overlapping_ticks[laser_type]:
                         self.laser_count += 1
                     else:
-                        slam_overlapping_ticks[laser_type].append(tick)
+                        slam_overlapping_ticks[laser_type].append(timepoint)
                 else:
                     self.laser_count += 1
 
